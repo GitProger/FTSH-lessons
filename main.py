2b6ff49424cceb3dd71b5ce02eedbfbd2c68dd30
GitProger <51924999+GitProger@users.noreply.github.com>
2019-10-30 12:29:31 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+class MainProgram:
+    def main(argv: list):
+        ...
+        return 0
+
+################################################################################
+##############################   Initialization   ##############################
+################################################################################
+if (__name__ == "__main__"):
+    import sys, os, math, random
+    retcode = MainProgram.main(sys.argv)
+    exit(retcode)
